66c0ab47c497c3f2726cb29bfea06d4634862d21
Kapil Chauhan <kapilchauhan@Sachins-MacBook-Air.local>
2025-02-08 14:54:53 +0530

    regain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return  <><h1>hello world this is kapil and liwansh welcome to our first next app</h1></>
+  return  <><h1>hello world this is kapil and liwansh welcome to our first next app hello hiiii</h1></>
 }
